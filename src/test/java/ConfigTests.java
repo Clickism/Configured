ae812e194fcfc0d4953033df56610a3f944760f0
Clickism <yagizaktasx@gmail.com>
2025-05-04 01:55:08 +0200

    rename to ConfigTests for consistency

--- src/test/java/ConfigTests.java
+++ src/test/java/ConfigTests.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ConfigTest {
+public class ConfigTests {
 
     @Test
     public void testDefaultSave(@TempDir Path tempDir) throws IOException {
