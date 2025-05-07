b48b32d03923e369f706f06cf07ff9d9fe6ccb8d
Clickism <yagizaktasx@gmail.com>
2025-05-08 01:50:43 +0200

    replace snakeyaml with jackson for YAML

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ java {
 
 dependencies {
     // Serialization
-    implementation("org.snakeyaml:snakeyaml-engine:2.9")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.0")
 
     // Annotations
     compileOnly("org.jetbrains:annotations:24.0.0")
