8d6bcea050a7f27ae36eed420857fdf39d0fc6a9
Clickism <yagizaktasx@gmail.com>
2025-05-07 18:13:49 +0200

    update example to use optionOf

--- src/test/java/ConfiguredExample.java
+++ src/test/java/ConfiguredExample.java
@@ -3,31 +3,28 @@ import me.clickism.configured.ConfigOption;
 
 public class ConfiguredExample {
     public static final Config CONFIG = Config.ofYaml("config.yml")
-            .version(1)
-            .header("""
-                    Example configuration file
-                    Using "Configured"!
-                    """);
+                    .version(1)
+                    .header("""
+                            Example configuration file
+                            Using "Configured"!
+                            """);
 
-    public static final ConfigOption<String> NAME = CONFIG.register(
-            ConfigOption.of("name", "John Smith")
+    public static final ConfigOption<String> NAME =
+            CONFIG.optionOf("name", "John Smith")
                     .description("Name of the user")
-                    .appendDefaultValue()
-    );
+                    .appendDefaultValue();
 
-    public static final ConfigOption<Integer> AGE = CONFIG.register(
-            ConfigOption.of("age", 18)
+    public static final ConfigOption<Integer> AGE =
+            CONFIG.optionOf("age", 18)
                     .description("Age of the user")
-                    .appendDefaultValue()
-    );
+                    .appendDefaultValue();
 
-    public static final ConfigOption<Boolean> STUDENT = CONFIG.register(
-            ConfigOption.of("student", true)
+    public static final ConfigOption<Boolean> STUDENT =
+            CONFIG.optionOf("student", true)
                     .description("Whether the user is a student or not")
-                    .appendDefaultValue()
-    );
+                    .appendDefaultValue();
 
     public static void main(String[] args) {
-        CONFIG.load();
+        CONFIG.save();
     }
 }
