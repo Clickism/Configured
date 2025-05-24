2288cf82ccd6b1b3052b3642c1eb2167a943654e
Clickism <yagizaktasx@gmail.com>
2025-05-24 18:44:01 +0200

    add static method to create key with parameters instead of annotations

--- src/main/java/me/clickism/configured/localization/LocalizationKey.java
+++ src/main/java/me/clickism/configured/localization/LocalizationKey.java
@@ -13,6 +13,20 @@ public interface LocalizationKey {
         };
     }
 
+    static LocalizationKey of(@NotNull String key, @NotNull String... parameters) {
+        return new LocalizationKey() {
+            @Override
+            public @NotNull String key() {
+                return key;
+            }
+
+            @Override
+            public String[] parameters() {
+                return parameters;
+            }
+        };
+    }
+
     default @NotNull String key() {
         return toString().toLowerCase().replace('$', '.');
     }
