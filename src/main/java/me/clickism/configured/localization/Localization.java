40758f0dc0c5681fbbff904c109b6ab4a9e1ede8
Clickism <yagizaktasx@gmail.com>
2025-05-25 16:29:25 +0200

    deploy localization files from resources

--- src/main/java/me/clickism/configured/localization/Localization.java
+++ src/main/java/me/clickism/configured/localization/Localization.java
@@ -8,15 +8,21 @@ import me.clickism.configured.format.YamlFormat;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.function.Function;
 
 /**
+ * todo: describe more about the localization system
  * Localization class for managing localized messages.
  */
 public class Localization {
-    private static final String PARAMETER_FORMAT = "{%s}";
+    private String parameterFormat = "{%s}";
 
     private final Function<String, File> fileGenerator;
     private final ConfigFormat format;
@@ -29,6 +35,8 @@ public class Localization {
     private Config fallbackConfig;
 
     private @Nullable Integer version;
+    private @Nullable ResourceProvider resourceProvider;
+    private boolean updateWithNewKeys = false;
 
     /**
      * Creates a new Localization instance with the specified format and file generator.
@@ -119,6 +127,25 @@ public class Localization {
         return version;
     }
 
+    private boolean isVersionMismatch() {
+        if (version == null) return false;
+        return config.currentVersion()
+                .map(version -> !version.equals(this.version))
+                .orElse(false);
+    }
+
+    /**
+     * Registers all keys from the given enum class that implements {@link LocalizationKey}.
+     * <p>
+     * You only need to register keys if you want to update local files with newer keys
+     * when there is a version mismatch.
+     * <p>
+     * For more information, see {@link Localization#updateWithNewKeys(boolean)}.
+     *
+     * @param enumClass the enum class containing localization keys
+     * @param <T>       the type of the enum, which must implement {@link LocalizationKey}
+     * @return this Localization instance
+     */
     public <T extends Enum<T> & LocalizationKey> Localization registerKeysFor(Class<T> enumClass) {
         for (Enum<T> enumConstant : enumClass.getEnumConstants()) {
             registerKey((LocalizationKey) enumConstant);
@@ -126,8 +153,38 @@ public class Localization {
         return this;
     }
 
+    /**
+     * Registers a single localization key.
+     * <p>
+     * You only need to register keys if you want to update local files with newer keys
+     * when there is a version mismatch.
+     * <p>
+     * For more information, see {@link Localization#updateWithNewKeys(boolean)}.
+     *
+     * @param key the localization key to register
+     * @return this Localization instance
+     */
     public Localization registerKey(LocalizationKey key) {
         options.add(ConfigOption.of(key.key(), key.key()));
+        return this;
+    }
+
+    /**
+     * Sets whether to update the localization files with new keys.
+     * <p>
+     * <strong>WARNING:</strong> If you set this to true, make sure to register all localization keys via
+     * {@link Localization#registerKeysFor(Class)} for enums or {@link Localization#registerKey(LocalizationKey)}
+     * for individual keys.
+     * <p>
+     * This will only affect local files that are not deployed from the resource directory.
+     * If the localization files are not local and can be deployed from the resource directory,
+     * they will always be updated with new keys regardless of this setting.
+     *
+     * @param updateWithNewKeys whether to update the localization files with new keys
+     * @return this Localization instance
+     */
+    public Localization updateWithNewKeys(boolean updateWithNewKeys) {
+        this.updateWithNewKeys = updateWithNewKeys;
         return this;
     }
 
@@ -138,7 +195,7 @@ public class Localization {
      *
      * <p>This method will not load/generate anything if no language is set.</p>
      *
-     * @return this localization instance.
+     * @return this Localization instance.
      */
     public Localization load() {
         if (language != null) {
@@ -149,9 +206,26 @@ public class Localization {
         if (fallbackLanguage != null && !fallbackLanguage.equals(language)) {
             fallbackConfig = createLanguageConfig(fallbackLanguage);
         }
-        config.load();
+        if (updateWithNewKeys) {
+            config.load();
+        } else {
+            config.loadWithoutUpdating();
+        }
+        if (isVersionMismatch()) {
+            if (resourceProvider != null) {
+                Configured.LOGGER.info("Version mismatch detected. Deploying from resource directory for '"
+                                       + language + "'");
+                String path = resourceProvider.pathGenerator.apply(language);
+                if (deploySingleResource(resourceProvider.clazz(), path, fileGenerator.apply(language).getPath())) {
+                    config.load();
+                }
+            } else {
+                Configured.LOGGER.warning("Version mismatch detected, but no resource directory set! "
+                                          + "Please ensure the localization files are up to date");
+            }
+        }
         if (fallbackConfig != null) {
-            fallbackConfig.load();
+            fallbackConfig.loadIfExistsWithoutUpdating();
         }
         return this;
     }
@@ -200,7 +274,7 @@ public class Localization {
         for (int i = 0; i < Math.min(params.length, paramNames.length); i++) {
             Object param = params[i];
             String paramName = paramNames[i];
-            String placeholder = String.format(PARAMETER_FORMAT, paramName);
+            String placeholder = String.format(parameterFormat, paramName);
             result = result.replace(placeholder, String.valueOf(param));
         }
         return result;
@@ -223,4 +297,88 @@ public class Localization {
         }
         return key.key();
     }
+
+    /**
+     * Sets the resource provider from which localization files can be deployed.
+     * <p>
+     * If you set a resource provider, the localization file(s) for the current
+     * language will be deployed from the resource path generated via the given
+     * path generator function. The localization file(s) will be deployed to the
+     * file path generated by the file generator function passed to the constructor.
+     * See {@link Localization#ofYaml(Function)} for more information.
+     * <p>
+     * The path generator function should take a language code as input and return the
+     * path to the localization file for that language.
+     * <p>
+     * For a path in your resources folder, i.E: <code>main/java/.../resources/</code>,
+     * make your path generator starts with <code>/</code> to ensure it starts from the root of the classpath.
+     * <p>
+     * Example usage: {@code .resourceDirectory(Main.class, lang -> "/locales/" + lang + ".yml");}
+     *
+     * @param clazz         the class from which the resource directory will be derived,
+     *                      usually the main class of your application.
+     * @param pathGenerator a function that generates the path to the localization file given a language code.
+     * @return this Localization instance
+     */
+    public Localization resourceProvider(Class<?> clazz, Function<String, String> pathGenerator) {
+        this.resourceProvider = new ResourceProvider(clazz, pathGenerator);
+        return this;
+    }
+
+    /**
+     * Retrieves the current parameter format used for placeholders in localized strings.
+     *
+     * @return the parameter format as a string
+     */
+    public String parameterFormat() {
+        return parameterFormat;
+    }
+
+    /**
+     * Sets the format for parameter placeholders in localized strings.
+     * Only change this if you want to use a different format for parameters in your localization strings.
+     * <p>
+     * Default is <code>{%s}</code>.
+     *
+     * @param parameterFormat the format string for parameter placeholders,
+     *                        where <code>%s</code> will be replaced by the parameter name.
+     * @return this Localization instance
+     */
+    public Localization parameterFormat(String parameterFormat) {
+        this.parameterFormat = parameterFormat;
+        return this;
+    }
+
+    /**
+     * Deploys a single resource from the specified class's resources to a given destination path.
+     * The method copies the resource from the classpath to the specified file path on the filesystem.
+     * <p>
+     * If the resource does not exist or an error occurs during the copy process, the method logs the error
+     * and returns false. If the operation is successful, it returns true.
+     *
+     * @param clazz           the class from whose resources the resource will be retrieved
+     * @param resourcePath    the path to the resource within the class's resources
+     * @param destinationPath the filesystem path where the resource should be deployed
+     * @return true if the resource was successfully deployed, false otherwise
+     */
+    protected boolean deploySingleResource(Class<?> clazz, String resourcePath, String destinationPath) {
+        Configured.LOGGER.info("Deploying resource '" + resourcePath + "' to '" + destinationPath + "'");
+        try (InputStream in = clazz.getResourceAsStream(resourcePath)) {
+            if (in == null) throw new FileNotFoundException("Resource not found: " + resourcePath
+                                                            + ". Local file will be used instead.");
+            File destinationFile = new File(destinationPath);
+            Files.createDirectories(destinationFile.getParentFile().toPath());
+            Files.copy(in, destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            return true;
+        } catch (IOException e) {
+            Configured.LOGGER.severe("Failed to deploy resource: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * A record representing a resource provider that can generate paths for localization files.
+     * It contains the class from which the resource will be retrieved and a function to generate the path.
+     */
+    private record ResourceProvider(Class<?> clazz, Function<String, String> pathGenerator) {}
 }
