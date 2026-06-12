296839da6ee7b274ffda073df0ff5eff673c4c54
Clickism <yagizaktasx@gmail.com>
2026-06-12 15:47:47 +0200

    fix test formatting

--- localization/src/test/java/LocalizationTest.java
+++ localization/src/test/java/LocalizationTest.java
@@ -23,7 +23,14 @@ public class LocalizationTest {
     private static Localization localization;
 
     enum Message implements Translatable {
-        @Parameters("username") USER_NOT_FOUND, CONFIGURATION_ERROR, INVALID_INPUT, @Parameters({"player", "action"}) OPERATION_SUCCESS, @Parameters({"reason", "details"}) OPERATION_FAILED;
+        @Parameters("username")
+        USER_NOT_FOUND,
+        CONFIGURATION_ERROR,
+        INVALID_INPUT,
+        @Parameters({"player", "action"})
+        OPERATION_SUCCESS,
+        @Parameters({"reason", "details"})
+        OPERATION_FAILED;
 
 
         @Override
@@ -35,7 +42,11 @@ public class LocalizationTest {
     @Test
     public void testLocalization(@TempDir Path tempDir) throws IOException {
         File file = tempDir.resolve("test.yml").toFile();
-        localization = Localization.of(lang -> file.getAbsolutePath()).version(2).fallbackLanguage("en_US").language("en_US");
+        localization =
+                Localization.of(lang -> file.getAbsolutePath())
+                        .version(2)
+                        .fallbackLanguage("en_US")
+                        .language("en_US");
         Files.writeString(file.toPath(), """
                 _version: 2
                 user_not_found: "User {username} could not be found."
@@ -45,17 +56,37 @@ public class LocalizationTest {
                 operation_failed: "Operation failed due to {reason}: {details}."
                 """);
         localization.load();
-        assertEquals("User Alice could not be found.", Message.USER_NOT_FOUND.get("Alice"));
-        assertEquals("There was an error in the configuration.", Message.CONFIGURATION_ERROR.get());
-        assertEquals("The input provided is invalid.", Message.INVALID_INPUT.get());
-        assertEquals("Bob has successfully logged in.", Message.OPERATION_SUCCESS.get("Bob", "logged in"));
-        assertEquals("Operation failed due to network error: Connection timed out.", Message.OPERATION_FAILED.get("network error", "Connection timed out"));
+        assertEquals(
+                "User Alice could not be found.",
+                Message.USER_NOT_FOUND.get("Alice")
+        );
+        assertEquals(
+                "There was an error in the configuration.",
+                Message.CONFIGURATION_ERROR.get()
+        );
+        assertEquals(
+                "The input provided is invalid.",
+                Message.INVALID_INPUT.get()
+        );
+        assertEquals(
+                "Bob has successfully logged in.",
+                Message.OPERATION_SUCCESS.get("Bob", "logged in")
+        );
+        assertEquals(
+                "Operation failed due to network error: Connection timed out.",
+                Message.OPERATION_FAILED.get("network error", "Connection timed out")
+        );
     }
 
     @Test
     public void testVersionMismatch(@TempDir Path tempDir) throws IOException {
         File file = tempDir.resolve("test.json").toFile();
-        localization = Localization.of(lang -> file.getAbsolutePath()).version(3).fallbackLanguage("en_US").language("en_US").resourceProvider(LocalizationTest.class, lang -> lang + ".json");
+        localization =
+                Localization.of(lang -> file.getAbsolutePath())
+                        .version(3)
+                        .fallbackLanguage("en_US")
+                        .language("en_US")
+                        .resourceProvider(LocalizationTest.class, lang -> lang + ".json");
         Files.writeString(file.toPath(), """
                 {
                     "_version": 1,
@@ -64,14 +95,21 @@ public class LocalizationTest {
                 """);
         // This should replace with the default value
         localization.load();
-        assertEquals("User Alice could not be found.", Message.USER_NOT_FOUND.get("Alice"));
+        assertEquals(
+                "User Alice could not be found.",
+                Message.USER_NOT_FOUND.get("Alice")
+        );
     }
 
     @Test
     public void testNoVersionMismatch(@TempDir Path tempDir) throws IOException {
         File file = tempDir.resolve("test.json").toFile();
-        localization = Localization.of(lang -> file.getAbsolutePath()).version(3) // File has version 3 as well
-                .fallbackLanguage("en_US").language("en_US").resourceProvider(LocalizationTest.class, lang -> lang + ".json");
+        localization =
+                Localization.of(lang -> file.getAbsolutePath())
+                        .version(3) // File has version 3 as well
+                        .fallbackLanguage("en_US")
+                        .language("en_US")
+                        .resourceProvider(LocalizationTest.class, lang -> lang + ".json");
         Files.writeString(file.toPath(), """
                 {
                     "_version": 3,
@@ -80,13 +118,21 @@ public class LocalizationTest {
                 """);
         // This should keep old vlaues
         localization.load();
-        assertEquals("This message should not be used.", Message.USER_NOT_FOUND.get("Alice"));
+        assertEquals(
+                "This message should not be used.",
+                Message.USER_NOT_FOUND.get("Alice")
+        );
     }
 
     @Test
     public void testVersionOverrideFromResource(@TempDir Path tempDir) throws IOException {
         File file = tempDir.resolve("test.json").toFile();
-        localization = Localization.of(lang -> file.getAbsolutePath()).version(3).fallbackLanguage("no_version").language("no_version").resourceProvider(LocalizationTest.class, lang -> lang + ".json");
+        localization =
+                Localization.of(lang -> file.getAbsolutePath())
+                        .version(3)
+                        .fallbackLanguage("no_version")
+                        .language("no_version")
+                        .resourceProvider(LocalizationTest.class, lang -> lang + ".json");
         // Should deploy new resource, and override _version to be the curent one
         localization.load();
 
